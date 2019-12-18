6c1c7293e6e4340625829cfb56f8fe0f967f46db
JohnUppercut <roma.andromed@yandex.ru>
2019-12-18 19:04:24 +0300

    Add Auto

---
+++ MiniHackathon/MiniHackathon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniHackathon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
