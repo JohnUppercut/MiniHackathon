7e82d529eb239fd43516a27af922e552c64e443b
NickyLordion <ya.sabog@yandex.ru>
2019-12-19 13:41:03 +0300

    Prob DB

---
+++ MiniHackathon/MiniHackathon/Models/DBcontext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MiniHackathon.Models
+{
+    public class DBcontext:DbContext
+    {
+        public DBcontext(DbContextOptions<DBcontext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Doctor>().ToTable("Doctor");
+        }
+    }
+
+}
---
+++ MiniHackathon/MiniHackathon/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniHackathon.Models
+{
+    public class User:IdentityUser
+    {
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string HashPass { get; set; }
+        public string FIO { get; set; }
+    }
+}
